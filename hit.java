97509194faad494b23b8180ea6477b1555400b9f
Seenivasan-M <seenikeys101@gmail.com>
2024-01-28 07:15:54 -0800

    yes

--- hit.java
+++ hit.java
@@ -1,7 +1,8 @@
 public class hit {
     public static void main(String []args){
         System.out.println("git file hit");
+        System.out.print("the hit man is not an enterpruner jorney");
+        System.out.print("In this case study we gonna talk about the business while sucessus or not \nso let get into the fuck in video ");
     }
-
     
 }
