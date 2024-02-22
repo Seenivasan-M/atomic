00b9fb8997af838f06256eb70d5e7eeb4c4f0ecf
Seenivasan-M <seenikeys101@gmail.com>
2024-02-22 06:25:29 -0800

    file changed

--- time.java
+++ time.java
@@ -1,43 +1,95 @@
-class wtime{
+import java.util.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    
 
-    public wtime(int i, int r) {
-        i=r;
-    } 
-     
-    public  void Wtime(){
-        for(int y=1;y<=5;y++){
-            for(int h= 1;h<=5;h++){
-            System.out.print("@");
-            }
-            System.out.println();
-        }
+//class
+
+class begin{
+    //
+    //method 
+    //
+
+void inhi(){
+
+        System.out.println("do want date or data \n just enter which one you want");
+        System.out.println("DATE AND TIME or DATA");
+
     }
 }
-// second class
-
- class fug{
-    int g;
-    int n;
-
-    public fug(){
-        g=34;
-        n=g+2;
-    }
-
-    
 
 
-}
 
 public class time{
-    public static void main(String []args){
+    public static void main(String ...args) throws Exception{
+        
 
-        wtime in=new wtime(23,34);
-        in.Wtime();
-        // System.out.println(in.wtime());
-      
+        begin var=new begin();
+        var.inhi();
+    System.out.println(var) ;
+
+        Scanner call=new Scanner(System.in);
+
+        String inp=call.nextLine();
+
+
+if(inp.equals("date")){
+        //date and time
+        LocalDateTime datetime=LocalDateTime.now();
+        DateTimeFormatter form=DateTimeFormatter.ofPattern("E \n MMM dd yyyy \n HH:mm:ss \n");
+        String type=datetime.format(form);
+        System.out.println(type);
+}
+        //
+        //
+        //ArrayList 
+        else if(inp.equals("data")){
+        ArrayList <Integer> ref=new ArrayList <Integer>();
+        ref.add(456156);
+        ref.add(456533);
+        ref.add(45612);
+        ref.add(45634);
+
+        for(int h:ref){
+            System.out.println(h);
+        }
+    
+        //
+        //d
+        //Arraylist2
+        ArrayList <Integer> ref2=new ArrayList <Integer>();
+        ref2.add(4568);
+        ref2.addAll(ref);
+
+        ref2.get(1);
+        ref2.remove(2);
+
+        ref2.set(2,8906);
+
+    // try
+    //catch
+        try{
+            ref2.remove(4);
+            }
+            catch(IndexOutOfBoundsException g) {
+                System.out.println("4 th array list has been removed \n \n \n");
+    
+            }
+
+    Collections.sort(ref2);
+
+        for(int loop:ref2){
+
+            
+            System.out.println(loop);
 
         }
+
+        ref2.clear();
+        }
+    
+call.close();
+    
+
     }
+}
